0b87a210e147b1632d068687f746df477b634584
shaunstummer <shaun_stummer@hotmail.com>
2014-09-23 14:38:32 +1000

    script changes
    script changes

--- myapp/www/js/app.js
+++ myapp/www/js/app.js
@@ -1,19 +1,107 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+angular.module('todo', ['ionic','firebase'])
+/**
+ * The Projects factory handles saving and loading projects
+ * from local storage, and also lets us save and load the
+ * last active project index.
+ */
+.factory('Projects', function() {
+  return {
+    all: function() {
+      var projectString = window.localStorage['projects'];
+      if(projectString) {
+        return angular.fromJson(projectString);
+      }
+      return [];
+    },
+    save: function(projects) {
+      window.localStorage['projects'] = angular.toJson(projects);
+    },
+    newProject: function(projectTitle) {
+      // Add a new project
+      return {
+        title: projectTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveProject']) || 0;
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] = index;
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+  }
 })
+
+.controller('TodoCtrl', function($scope, $firebase, $timeout, Modal, Projects) {
+
+  $scope.projectsList = {};
+  $scope.user = {};
+  // Load or initialize projects
+  $scope.projectsList = $firebase(new Firebase("https://incandescent-inferno-6213.firebaseio.com/projects"));
+  $scope.user = $firebase(new Firebase("https://incandescent-inferno-6213.firebaseio.com/Users/John"));
+
+  $scope.projectsList.$on("loaded", function() {
+      console.log($scope.projectsList);
+      window.projectsList = $scope.projectsList;
+  });
+
+  $scope.projectsList.$on("loaded", function() {
+      console.log($scope.user);
+      window.user = $scope.user;
+  });
+  
+  // A utility function for creating a new project
+  // with the given projectTitle
+  var createProject = function(projectTitle) {
+      $scope.projectsList[projectTitle] = [];
+      $scope.projectsList.$save(projectTitle);
+      $scope.selectProject(projectTitle);
+  }
+
+  // Called to create a new project
+  $scope.newProject = function() {
+      var projectTitle = prompt('Project name');
+      if(projectTitle) {
+          createProject(projectTitle);
+      }
+  };
+
+  // Called to select the given project
+  $scope.selectProject = function(project) {
+      $scope.user.lastproject = project;
+      $scope.sideMenuController.close();
+  };
+
+  // Create our modal
+  Modal.fromTemplateUrl('new-task.html', function(modal) {
+      $scope.taskModal = modal;
+  }, {
+      scope: $scope
+  });
+
+  $scope.createTask = function(task) {
+      if(!$scope.user.lastproject) {
+          return;
+      }
+
+      var name = $scope.user.lastproject;
+      $scope.projectsList[name].push(task.title);
+      $scope.projectsList.$save(name);
+      $scope.taskModal.hide();
+
+      task.title = "";
+  };
+
+  $scope.newTask = function() {
+      $scope.taskModal.show();
+  };
+
+  $scope.closeNewTask = function() {
+      $scope.taskModal.hide();
+  }
+
+  $scope.toggleProjects = function() {
+      $scope.sideMenuController.toggleLeft();
+  };
+
+});
